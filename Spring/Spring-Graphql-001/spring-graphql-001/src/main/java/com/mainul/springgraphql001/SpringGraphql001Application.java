c02638f38f1fbf9ae5e46bb7ae91a8944022e430
mainuldip <mainuldip@gmail.com>
2024-03-17 00:09:24 -0400

    java spring-boot graphq init

---
+++ Spring/Spring-Graphql-001/spring-graphql-001/src/main/java/com/mainul/springgraphql001/SpringGraphql001Application.java
@@ -0,0 +1,13 @@
+package com.mainul.springgraphql001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphql001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphql001Application.class, args);
+	}
+
+}
